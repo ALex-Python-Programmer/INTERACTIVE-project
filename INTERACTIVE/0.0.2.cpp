fcfb81ddcaece76e4372a8c9b16192bbe90699b9
Alex-Github-Programmer <55904367+Alex-Github-Programmer@users.noreply.github.com>
2020-06-19 16:07:43 +0800

    Update and rename 0.0.0a3 2019y334d.cpp to 0.0.2.cpp

--- INTERACTIVE/0.0.2.cpp
+++ INTERACTIVE/0.0.2.cpp
@@ -25,7 +25,7 @@
 #define YES 0x00000002
 
 char target[1024];
-char version[]="0.0.0a3 2019y334d";
+char version[]="0.0.2";
 char help[]="INTERACTIVE help document:"n
 "    This file is the sourse code of the INTERACTIVE project."n
 "    The argument values(argv) stands for:"n
